326e11248919dbdfe99cbb7f42dcfc52757910e5
Suneet Srivastava <suneetbond91@gmail.com>
2018-08-03 21:13:04 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# dirty_dart
+
+Get your hands dirty with Flutter.
+<3 by codedsun
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ android/app/src/main/java/codedsun/com/dirtydart/MainActivity.java
@@ -0,0 +1,14 @@
+package codedsun.com.dirtydart;
+
+import android.os.Bundle;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
